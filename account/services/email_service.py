7b47479841983a1ba50df37d3d706068f9ca2272
Limah-T <limahtechnology@yahoo.com>
2026-07-30 16:30:34 +0100

    change: Change SMTP connection to HTTP using API key for fast delivery.

--- account/services/email_service.py
+++ account/services/email_service.py
@@ -1,32 +1,32 @@
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-import os
+from nhcc_operations.config.settings.base import env
+from requests.exceptions import RequestException
+import requests
              
-SENDER_EMAIL = os.environ.get("SENDER_EMAIL") 
+SENDER_EMAIL = env("SENDER_EMAIL") 
+URL = env("SEND_EMAIL_URL")
+API_KEY = env("EMAIL_API_KEY")
 
+headers = {
+    "api-key": API_KEY,
+    "Content-Type": "application/json"
+}
 
 def send_mail(subject, receiver, context:dict, html_file, txt_file):
+
     try:
-        text_message = render_to_string(txt_file, context)
-        html_message = render_to_string(html_file, context)
-
-        email = EmailMultiAlternatives(
-            subject=subject,
-            body=text_message,
-            from_email=SENDER_EMAIL,
-            to=[receiver],
-        )
-
-        email.attach_alternative(
-            html_message,
-            "text/html",
-        )
-
-        email.send(fail_silently=False)
+        html = render_to_string(template_name=html_file,context=context)
+        text = render_to_string(template_name=txt_file,context=context)
+        payload = {
+            "sender": {"email": SENDER_EMAIL, "name": "NHCC Operations"},
+            "subject": subject, "to": [{"email": receiver}],
+            "htmlContent": html, "textContent": text}
+    except Exception as exc:
+        return False   
+    try:
+        response = requests.post(URL, json=payload, headers=headers, timeout=20)
+        response.raise_for_status()
         print("Email sent successfully!")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
+    except RequestException as exc:
+        print(f"An error occurred: {exc}")
 
